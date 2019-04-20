1a50a9c64347d9f45142c42fedc3080bdf39a31d
saulwolfcode <43302778+saulwolfcode@users.noreply.github.com>
2019-04-20 00:30:57 -0300

    Entorno estable sass landinPage

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 
+const showMenu  = (toggleId,navId)=>{
+const toggle = document.getElementById (toggleId),
+      nav = document.getElementById (navId)
+      if(toggle && nav){
+        toggle.addEventListener("click",()=>{
+              nav.classList.toggle("show")
+          })
+      }
+}
+showMenu("main-menu-hamburger","main-nav")
 
-const getScrollBarWidth=()=>innerWidth -
-document.documentElement.clientWidth
-document.documentElement.style.setProperty("--scroll-bar",getScrollBarWidth())
+// const getScrollBarWidth=()=>innerWidth -
+// document.documentElement.clientWidth
+// document.documentElement.style.setProperty("--scroll-bar",getScrollBarWidth())
